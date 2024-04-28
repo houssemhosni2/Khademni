7e5e5c005af3c93bd8213639ba168f01c79ea277
zeineb <zaineb.benaziza@esprit.tn>
2024-04-28 21:40:10 +0100

    first

--- src/main/java/com/example/khademni/entity/User.java
+++ src/main/java/com/example/khademni/entity/User.java
@@ -1,33 +1,50 @@
 package com.example.khademni.entity;
 
 import jakarta.persistence.*;
-
-import java.util.List;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
-@ToString
-
+@AllArgsConstructor
 @Entity
-public class User {
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nomUtilisateur;
-    private String motDePasse;
+    private long id;
+    private String firstname;
+    private String lastname;
     private String email;
-    private String profile;
-    @ElementCollection
-    private List<String> competences;
-
-    private String nomEntreprise;
-    private String adresseEntreprise;
+    private String password;
+    private String profil;
+    private boolean accountLocked;
+    private boolean enabled;
+//    private boolean accountLocked;
+//    private boolean enabled;
+//    @CreatedDate
+//    @Column(nullable = false,updatable = false)
+//    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+//    @ManyToMany(fetch = FetchType.EAGER)
+//    private List<Role> roles;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Evaluation> evaluations;
@@ -41,4 +58,40 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Paiement> paiements;
 
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    public String fullName(){
+      return firstname + " " +lastname;
+    }
 }
